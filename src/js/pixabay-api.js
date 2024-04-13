3d135aa5ad7ea8bb09517c29dc531840c577dd42
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-04-13 16:47:11 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,16 @@
 
-
+import axios  from "axios";
 // https://pixabay.com/api/?key=43264129-0a20e73c7f6f9c0a58d1fd1ca&q=dog&image_type=photo&orientation=horizontal&safesearch=true
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43264129-0a20e73c7f6f9c0a58d1fd1ca';
 
 
-import axios  from "axios";
+
 // console.log(axios);
 
 
 export async function makeRequest(inputEl) {
  try{ 
-//     const params = new URLSearchParams({
-//     key: API_KEY,
-//     q: `${inputEl}`,
-//     imege_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//   });
   const response = await axios.get(`${BASE_URL}`,{params: {
     key: API_KEY,
     q: `${inputEl}`,
@@ -30,10 +23,3 @@ export async function makeRequest(inputEl) {
     alert(error);
 }
 }
-//   return fetch(`${BASE_URL}?${params}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
