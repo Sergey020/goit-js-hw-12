91d7299dfa87990d191462269bcb82599a5f7742
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-04-14 22:10:29 +0200

    pagination

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,24 @@
-
-import axios  from "axios";
+import axios from 'axios';
 // https://pixabay.com/api/?key=43264129-0a20e73c7f6f9c0a58d1fd1ca&q=dog&image_type=photo&orientation=horizontal&safesearch=true
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43264129-0a20e73c7f6f9c0a58d1fd1ca';
-let page = 1 ;
 
 
-
-
-
-export async function makeRequest(inputEl) {
- try{ 
-  const response = await axios.get(`${BASE_URL}`,{params: {
-    key: API_KEY,
-    q: `${inputEl}`,
-    imege_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 15,
-    page 
-
-  }});
-  return response.data;
-      }catch(error){
+export async function makeRequest(inputEl, page) {
+  try {
+    const response = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: `${inputEl}`,
+        imege_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 15,
+        page,
+      },
+    });
+    return response.data;
+  } catch (error) {
     alert(error);
-}
+  }
 }
--- src/main.js
+++ src/main.js
@@ -4,22 +4,28 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { createMarkup } from './js/render-functions';
 import { makeRequest } from './js/pixabay-api';
+
 const searchEl = document.querySelector('form');
-const ulEl = document.querySelector('.gallery');
+const ulEl = document.querySelector('ul');
 const loaderEl = document.querySelector('.loader');
 const btnMore = document.querySelector('.btn');
-searchEl.addEventListener('submit', handleSubmit);
 
-let inputEl = "";
+
+
+let inputEl = '';
+let page = 1;
+let deltaPage;
 
 btnMore.style.display = 'none';
 loaderEl.style.display = 'none';
+ 
+searchEl.addEventListener('submit', handleSubmit);
 
-
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  const inputEl = form.elements.imgName.value;
+ page = 1;
+  inputEl = form.elements.imgName.value;
   console.log(inputEl);
   if (inputEl === '') {
     iziToast.show({
@@ -35,33 +41,94 @@ function handleSubmit(event) {
 
   loaderEl.style.display = 'block';
 
-  makeRequest(inputEl)
-    .then(data => {
-      console.log(data.hits);
 
-      loaderEl.style.display = 'none';
-      if (data.hits.length === 0) {
-        ulEl.innerHTML = '';
-        iziToast.show({
-          message:
-            ' Sorry, there are no images matching your search query. Please try again! ',
-          position: 'topRight',
-          backgroundColor: 'red',
-          messageColor: '#FFFFFF',
-          transitionIn: 'fadeln',
-          timeout: 4000,
-        });
-        return;
-      }
-      ulEl.innerHTML = createMarkup(data.hits);
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
+  try {
+    const data = await makeRequest(inputEl, page);
+    console.log(data.hits);
+
+    // loaderEl.style.display = 'none';
+    if (data.hits.length === 0) {
+      iziToast.show({
+        message:
+          ' Sorry, there are no images matching your search query. Please try again! ',
+        position: 'topRight',
+        backgroundColor: 'red',
+        messageColor: '#FFFFFF',
+        transitionIn: 'fadeln',
+        timeout: 4000,
       });
-    })
-    .catch(error => console.log('catch', error))
-    .finally(() => form.reset());
+      loaderEl.style.display = 'none';
+      btnMore.style.display = 'none';
+      ulEl.innerHTML = '';
+      form.reset();
+      return;
+    }
+    loaderEl.style.display = 'none';
+    ulEl.innerHTML = createMarkup(data.hits);
+    btnMore.style.display = 'block';
+    deltaPage = Math.ceil(data.totalHits / data.hits.length);
+    if (page >= deltaPage){
+      btnMore.style.display = 'none'
+      iziToast.show({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+        backgroundColor: 'green',
+        messageColor: '#FFFFFF',
+        transitionIn: 'fadeln',
+        timeout: 4000,
+      });
+    }
+    // console.log(ulEl);
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+    lightbox.refresh();
+    form.reset();
+  } catch (error) {
+    alert(error);
+  }
 }
-
-
 btnMore.addEventListener('click', handleClick);
+
+async function handleClick() {
+  btnMore.style.display = 'none';
+  loaderEl.style.display = 'block';
+  page += 1;
+
+  try {
+    const data = await makeRequest(inputEl, page);
+    console.log(data.hits.length);
+    loaderEl.style.display = 'none';
+    ulEl.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+    btnMore.style.display = 'block'; //console.log(page);
+    if (page >= deltaPage) {
+      btnMore.style.display = 'none';
+      iziToast.show({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+        backgroundColor: 'green',
+        messageColor: '#FFFFFF',
+        transitionIn: 'fadeln',
+        timeout: 4000,
+      });
+    }
+
+    const card = document.querySelector('.gallery-link');
+    const cardHeight = card.getBoundingClientRect().height;
+
+    window.scrollBy({
+      left: 0,
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+    lightbox.refresh();
+  } catch (error) {
+    alert(error);
+  }
+}
